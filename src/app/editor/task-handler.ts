cc45d9931616789fe078055793fdb0c66acc9995
tuuling <reedik.tuuling@gmail.com>
2019-08-15 12:44:07 +0300

    Task inputs updated

--- src/app/editor/task-handler.ts
+++ src/app/editor/task-handler.ts
@@ -1,11 +1,12 @@
 import * as Viewer from 'bpmn-js/lib/NavigatedViewer';
 
-import { ElementsHandler } from "./elements-handler";
+import { ElementsHandler } from './elements-handler';
 
 declare let $: any;
 declare let CodeMirror: any;
 
-declare function require(name:string);
+declare function require(name: string);
+
 let config = require('../../config.json');
 
 let schemaCodeMirror;
@@ -42,7 +43,7 @@ export class TaskHandler {
   }
 
   getTaskInputQuery() {
-    let query = "";
+    let query = '';
     if (this.task.sqlTaskInfo != null) {
       let savedData = JSON.parse(this.task.sqlTaskInfo);
       query = savedData.input1;
@@ -51,7 +52,7 @@ export class TaskHandler {
   }
 
   getTaskSchema() {
-    let query = "";
+    let query = '';
     if (this.task.sqlTaskInfo != null) {
       let savedData = JSON.parse(this.task.sqlTaskInfo);
       query = savedData.schema;
@@ -100,35 +101,46 @@ export class TaskHandler {
     this.beingEdited = true;
     this.taskOptionsPanelContainer = $('#task-options-panel');
 
-    let taskName = "undefined";
+    let taskName = 'undefined';
     if (this.task.name) {
       taskName = this.task.name;
     }
     this.taskOptionsPanelContainer.find('.task-name').text(taskName);
-    
+
     let savedData;
-    let input1 = "";
+    let input1 = '';
+    let schema = '';
     if (this.task.sqlTaskInfo != null) {
       savedData = JSON.parse(this.task.sqlTaskInfo);
       input1 = savedData.input1;
+      schema = savedData.schema;
     }
-    this.taskOptionsPanelContainer.find('#task-input1').val(input1);
+    this.taskOptionsPanelContainer.find('#task-schema').val(schema);
+    this.taskOptionsPanelContainer.find('#task-query').val(input1);
 
     if (!this.elementsHandler.canEdit) {
       this.taskOptionsPanelContainer.find('.panel-footer').hide();
     }
 
     $('.task-options-panel, .data-object-options-panel').find('.CodeMirror').remove();
-    codeMirror1 = CodeMirror.fromTextArea(document.getElementById("task-input1"), {
+    schemaCodeMirror = CodeMirror.fromTextArea(document.getElementById('task-schema'), {
       //mode: "text/x-sql, text/x-mysql, text/x-mariadb, text/x-cassandra, text/x-plsql, text/x-mssql, text/x-hive, text/x-pgsql, text/x-gql, text/x-gpsql, text/x-esper",
-      mode: "text/x-mysql",
+      mode: 'text/x-mysql',
       readOnly: !this.elementsHandler.canEdit,
       lineNumbers: true,
       showCursorWhenSelecting: true
     });
-    setTimeout(function() {
+    codeMirror1 = CodeMirror.fromTextArea(document.getElementById('task-query'), {
+      //mode: "text/x-sql, text/x-mysql, text/x-mariadb, text/x-cassandra, text/x-plsql, text/x-mssql, text/x-hive, text/x-pgsql, text/x-gql, text/x-gpsql, text/x-esper",
+      mode: 'text/x-mysql',
+      readOnly: !this.elementsHandler.canEdit,
+      lineNumbers: true,
+      showCursorWhenSelecting: true
+    });
+    setTimeout(function () {
+      schemaCodeMirror.refresh();
       codeMirror1.refresh();
-     }, 10);
+    }, 10);
 
     this.highlightTaskInputAndOutputObjects();
     this.canvas.addMarker(this.task.id, 'selected');
@@ -142,36 +154,89 @@ export class TaskHandler {
 
   }
 
-  getPreparedQuery() {
+  getPreparedSchema() {
     let savedData: any = "";
-    let input = "";
+    let schema = "";
+    if (this.task.sqlTaskInfo != null) {
+      savedData = JSON.parse(this.task.sqlTaskInfo);
+      schema = savedData.schema;
+    }
+    if (schema) {
+      let result = this.elementsHandler.pg_parser.parse(schema);
+      if (result.parse_tree.length) {
+        if (result.parse_tree[0].CreateStmt) {
+          let tableName = result.parse_tree[0].CreateStmt.relation.RangeVar.relname;
+          return { success: { id: this.task.id, tableName: tableName, schema: schema } };
+
+        } else {
+          return { error: result.error.message };
+        }
+      } else {
+        return { error: result.error.message };
+      }
+    } else {
+      return { error: "Invalid schema statement" };
+    }
+  }
+
+  getTaskNameFromSchema() {
+    if (this.task.sqlTaskInfo != null) {
+      let savedData = JSON.parse(this.task.sqlTaskInfo);
+      if (savedData.schema) {
+        let result = this.elementsHandler.pg_parser.parse(savedData.schema);
+        if (result.parse_tree.length) {
+          if (result.parse_tree[0].CreateStmt.relation.RangeVar.relname) {
+            return result.parse_tree[0].CreateStmt.relation.RangeVar.relname;
+          }
+        }
+      }
+    }
+    return null;
+  }
+
+  getPreparedQuery() {
+    let savedData: any = '';
+    let input = '';
     if (this.task.sqlTaskInfo != null) {
       savedData = JSON.parse(this.task.sqlTaskInfo);
       input = savedData.input1;
     }
     if (input) {
-      let result = this.elementsHandler.pg_parser.parse(input);
-      if (result.parse_tree.length) {
-        if (result.parse_tree[0].CreateFunctionStmt) {
-          let stprocBody = result.parse_tree[0].CreateFunctionStmt.options[0].DefElem.arg[0].String.str;
-          let obj_query = stprocBody.trim().replace(/\s+/g, " ");
-          if (obj_query.substr(obj_query.length - 1) !== ";") {
-            obj_query += "\n;";
-          }
-          let innerResult = this.elementsHandler.pg_parser.parse(obj_query);
-          if (innerResult.parse_tree.length) {
-            return {success: {id: this.task.id, taskName: this.task.name.trim().replace(/\s+/g, "_"), query: obj_query}};
+      if (input.indexOf('$$') !== -1) {
+        let result = this.elementsHandler.pg_parser.parse(input);
+        if (result.parse_tree.length) {
+          if (result.parse_tree[0].CreateFunctionStmt) {
+            let stprocBody = result.parse_tree[0].CreateFunctionStmt.options[0].DefElem.arg[0].String.str;
+            let obj_query = stprocBody.trim().replace(/\s+/g, ' ');
+            if (obj_query.substr(obj_query.length - 1) !== ';') {
+              obj_query += '\n;';
+            }
+            let innerResult = this.elementsHandler.pg_parser.parse(obj_query);
+            if (innerResult.parse_tree.length) {
+              return { success: { id: this.task.id, taskName: this.task.name.trim().replace(/\s+/g, '_'), query: obj_query } };
+            } else {
+              return { error: innerResult.error.message };
+            }
           } else {
-            return {error: innerResult.error.message};
+            return { error: result.error.message };
           }
         } else {
-          return {error: result.error.message};
+          return { error: result.error.message };
         }
       } else {
-        return {error: result.error.message};
+        let result = this.elementsHandler.pg_parser.parse(input);
+        if (result.parse_tree.length) {
+          if (result.parse_tree[0].SelectStmt) {
+            return { success: { id: this.task.id, taskName: this.task.name.trim().replace(/\s+/g, '_'), query: input } };
+          } else {
+            return { error: result.error.message };
+          }
+        } else {
+          return { error: result.error.message };
+        }
       }
     } else {
-      return {error: "Stored procedure not found"};
+      return { error: 'Query not found' };
     }
   }
 
@@ -201,7 +266,7 @@ export class TaskHandler {
   }
 
   updateTaskOptions() {
-    let infoObj = {input1: codeMirror1.getValue()};
+    let infoObj = { input1: codeMirror1.getValue() };
     this.task.sqlTaskInfo = JSON.stringify(infoObj);
   }
 
@@ -211,7 +276,7 @@ export class TaskHandler {
     this.setNewModelContentVariableContent();
     this.canvas.removeMarker(this.task.id, 'highlight-general-error');
   }
-  
+
   removeTaskOptions() {
     this.terminateTaskOptionsEditProcess();
     delete this.task.sqlTaskInfo;
@@ -311,7 +376,7 @@ export class TaskHandler {
       }
       for (let obj of allInputsOutputs) {
         if (allInputs.indexOf(obj) !== -1 && allOutputs.indexOf(obj) !== -1 && objects.indexOf(obj) === -1) {
-           objects.push(obj);
+          objects.push(obj);
         }
       }
     }
